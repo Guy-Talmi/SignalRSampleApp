d38d41ce642eb9dc9a81e3456d5f4c93276d5107
Guy Talmi <gtalmi@diptechmachinery.com>
2023-05-17 19:13:40 +0300

    Add UserHub.

---
+++ SignalRSampleApp/Hubs/UserHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRSampleApp.Hubs;
+
+public class UserHub : Hub
+{
+	public static int TotalViews { get; set; } = 0;
+
+	public async Task NewWindowLoaded()
+	{
+		TotalViews++;
+
+		// Send the new total to all clients
+		await Clients.All.SendAsync("updateTotalViews", TotalViews);
+	}
+}
